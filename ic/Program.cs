676d6b1dfbab58ff383b26a4209b27549c552328
jems238 <fu-yoshida@j-ems.jp>
2020-05-21 17:11:18 +0900

    what?

--- ic/Program.cs
+++ ic/Program.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-
-namespace intCalc
+namespace mycalc
 {
     class Program
     {
@@ -13,85 +12,67 @@ namespace intCalc
         {
             try
             {
-                string arg = Console.ReadLine();
-                string[] arr = arg.Split(' ');
-                int int1;
-                int int2;
-
                 //引数の個数の確認
-                if (arr.Length != 4)
+                if (args.Length == 3
+                    && Regex.IsMatch(args[0], "^[0-9]+$")
+                    && Regex.IsMatch(args[2], "^[0-9]+$")
+                 )
                 {
-                    throw new ArgumentException();
-                }
-
-                //最初の引数がmycalcであることの確認
-                if (arr[0] != "mycalc")
-                {
-                    throw new ArgumentException();
-                }
-
-                //2つ目の引数がint型に収まる整数値であるかの確認
-                if (Regex.IsMatch(arr[1], "^[0-9]+$"))
-                {
-                    int1 = int.Parse(arr[1]);
+                    calc(int.Parse(args[0]), int.Parse(args[2]), args[1]);
                 }
                 else
                 {
-                    throw new ArgumentException();
+                    Console.WriteLine("以下のように入力してください。");
+                    Console.WriteLine("mycalc (整数値) (四則演算子) (整数値)");
                 }
-
-                //4つ目の引数がint型に収まる整数値であることの確認
-                if (Regex.IsMatch(arr[3], "^[0-9]+$"))
-                {
-                    int2 = int.Parse(arr[3]);
-                }
-                else
-                {
-                    throw new ArgumentException();
-                }
-
-                //3つ目の引数値によって、どんな演算がされるかが決まる。
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("引数にオーバーフローが発生しました。");
+            }
+        }
+        static void calc(int int1, int int2, string ope)
+        {
+            try
+            {
                 checked
                 {
-                    var ope = arr[2];
                     int ans;
-
                     switch (ope)
                     {
-                        case "/":
-                            int ansQuo = int1 / int2;
-                            int ansRem = int1 % int2;
-                            Console.WriteLine($"{int1} * {int2} = {ansQuo} ({ansRem})");
+                        case "+":
+                            ans = int1 + int2;
+                            Console.WriteLine($"{int1} + {int2} = {ans}");
+                            break;
+                        case "-":
+                            ans = int1 - int2;
+                            Console.WriteLine($"{int1} - {int2} = {ans}");
                             break;
                         case "*":
                             ans = int1 * int2;
                             Console.WriteLine($"{int1} * {int2} = {ans}");
                             break;
-                        case "+":
-                            ans = int1 + int2;
-                            Console.WriteLine($"{int1} * {int2} = {ans}");
-                            break;
-                        case "-":
-                            ans = int1 - int2;
-                            Console.WriteLine($"{int1} * {int2} = {ans}");
+                        case "/":
+                            if (int2 != 0)
+                            {
+                                int ansQuo = int1 / int2;
+                                int ansRem = int1 % int2;
+                                Console.WriteLine($"{int1} / {int2} = {ansQuo} ({ansRem})");
+                            }
+                            else
+                            {
+                                Console.WriteLine("0で割ることはできません。");
+                            }
                             break;
                         default:
-                            throw new ArgumentException();
+                            Console.WriteLine("2つ目の引数には+,-,*,/のいづれかを入力してください。");
+                            break;
                     }
                 }
             }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("以下のように入力してください。");
-                Console.WriteLine("mycalc (整数値) (四則演算子) (整数値)");
-            }
             catch (OverflowException)
             {
-                Console.WriteLine("オーバーフローが発生しました。");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("0で割ることはできません。");
+                Console.WriteLine("計算結果にオーバーフローが発生しました。");
             }
         }
     }
