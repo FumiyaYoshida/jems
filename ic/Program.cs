c5ed998d582b57d150a685e94bf84915be775d3f
jems238 <fu-yoshida@j-ems.jp>
2020-05-22 15:50:09 +0900

    ic9=743 complete

--- ic/Program.cs
+++ ic/Program.cs
@@ -13,72 +13,85 @@ namespace mycalc
             string usage = @"2つの整数を計算します。
 >mycalc 整数1 演算子(+|-|*|/) 整数2
 例) 2 + 3";
-            string errorMessage;
+            string errorMessage = null;
             if (args.Length != 3)
             {
                 errorMessage = usage;
             }
             else
             {
-                if (ValidateOperand(args[0], out int int1, out errorMessage)
-                    && ValidateOperater(args[1], out string ope, out errorMessage)
-                    && ValidateOperand(args[2], out int int2, out errorMessage)
-                    && ValidatePair(ope, int2, out errorMessage)
+                string ope = args[1];
+                if (ValidateOperand(args[0], out int a, out errorMessage)
+                    && ValidateOperater(ope, out errorMessage)
+                    && ValidateOperand(args[2], out int b, out errorMessage)
                  )
                 {
-                    Calc(int1, int2, args[1]);
+                    int result = Calc(a, b, ope, out int surplus, out errorMessage);
+                    if (string.IsNullOrEmpty(errorMessage))
+                    {
+                        Console.WriteLine($"{a} {ope} {b} = {result}" + ((surplus != 0) ? $" ({surplus})" : ""));
+                    }
                 }
             }
-
             if (!string.IsNullOrEmpty(errorMessage))
             {
                 Console.Error.WriteLine(errorMessage);
             }
         }
-        static void Calc(int int1, int int2, string ope)
+        /// <summary>
+        /// 四則演算の計算を行う
+        /// </summary>
+        /// <param name="a">一つ目の整数</param>
+        /// <param name="b">二つ目の整数</param>
+        /// <param name="ope">四則演算子</param>
+        /// <param name="surplus">割り算の余り。割り算以外の時は0が入っており、で利用されない。</param>
+        /// <param name="errorMessage">NGの場合はエラーメッセージ、OKの場合はnullが返る。</param>
+        /// <returns>計算結果</returns>
+        static int Calc(int a, int b, string ope, out int surplus, out string errorMessage)
         {
+            surplus = 0;
+            errorMessage = null;
             try
             {
                 checked
                 {
-                    int ans;
-                    string output;
                     switch (ope)
                     {
                         case "+":
-                            ans = int1 + int2;
-                            output = $"{int1} + {int2} = {ans}";
-                            break;
+                            return a + b;
                         case "-":
-                            ans = int1 - int2;
-                            output = $"{int1} - {int2} = {ans}";
-                            break;
+                            return a - b;
                         case "*":
-                            ans = int1 * int2;
-                            output = $"{int1} * {int2} = {ans}";
-                            break;
+                            return a * b;
                         case "/":
-                            int ansQuo = int1 / int2;
-                            int ansRem = int1 % int2;
-                            output = $"{int1} / {int2} = {ansQuo} ({ansRem})";
+                            if (b == 0)
+                            {
+                                errorMessage = "割り算の場合、割る数に0を指定できません。";
+                            }
+                            else
+                            {
+                                surplus = a % b;
+                                return a / b;
+                            }
                             break;
                         default:
                             throw new FormatException();
                     }
-                    Console.WriteLine(output);
                 }
             }
             catch (OverflowException)
             {
-                Console.Error.WriteLine("異常が発生しました。プログラムを終了します。" +
-                    "\r\n（計算でオーバーフローが発生しました。）");
+                errorMessage = "計算結果でオーバーフローが発生しました。";
             }
-            catch (FormatException)
-            {
-                Console.Error.WriteLine("異常が発生しました。プログラムを終了します。" +
-                    "\r\n（四則演算子の判定でエラーが発生しました。）");
-            }
+            return 0;
         }
+        /// <summary>
+        /// 入力文字の整数値の検証
+        /// </summary>
+        /// <param name="operand">検査対象の文字列</param>
+        /// <param name="value">NGの場合は0、OKの場合は検査対象の文字列をint型に変換したものが返る。</param>
+        /// <param name="errorMessage">NGの場合はエラーメッセージ、OKの場合はnullが返る。</param>
+        /// <returns>true:OK, false:NG</returns>
         static bool ValidateOperand(string operand, out int value, out string errorMessage)
         {
             value = 0;
@@ -88,48 +101,37 @@ namespace mycalc
                 try
                 {
                     value = int.Parse(operand);
+                    return true;
                 }
-                catch
+                catch (OverflowException)
                 {
-                    errorMessage = $"引数でオーバーフローが発生しました。";
-                    return false;
+                    errorMessage = "引数でオーバーフローが発生しました。";
                 }
-                return true;
             }
             else
             {
                 errorMessage = "正しい整数値を指定してください。";
-                return false;
             }
+            return false;
         }
-        static bool ValidateOperater(string operand, out string value, out string errorMessage)
+        /// <summary>
+        /// 入力文字列の四則演算子の検証
+        /// </summary>
+        /// <param name="ope">検査対象の文字列</param>
+        /// <param name="errorMessage">NGの場合はエラーメッセージ、OKの場合はnullが返る。。</param>
+        /// <returns>true:OK, false:NG</returns>
+        static bool ValidateOperater(string ope, out string errorMessage)
         {
-            value = "";
             errorMessage = null;
-            if (Regex.IsMatch(operand, "^\\+$|^-$|^\\*$|^/$"))
+            if (Regex.IsMatch(ope, "^\\+$|^-$|^\\*$|^/$"))
             {
-
-                value = operand;
                 return true;
             }
             else
             {
                 errorMessage = "2つ目の引数には四則演算子(+,-,*,/)を入力してください。";
-                return false;
             }
-        }
-        static bool ValidatePair(string ope, int int2, out string errorMessage)
-        {
-            errorMessage = null;
-            if (!(ope == "/" && int2 == 0))
-            {
-                return true;
-            }
-            else
-            {
-                errorMessage = "割り算の場合、割る数に0を指定できません。";
-                return false;
-            }
+            return false;
         }
     }
 }
