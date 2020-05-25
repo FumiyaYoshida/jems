15975025115ca1cd6d1bc0ceb661f05422ed2a9b
jems238 <fu-yoshida@j-ems.jp>
2020-05-25 17:59:25 +0900

    cp1= 747 complete step1

---
+++ cp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+namespace mycalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream inputStream = Console.OpenStandardInput();
+            using (var reader = new StreamReader(inputStream))
+            {
+                int readChar;
+                while ((readChar = reader.Read()) != -1)
+                {
+                    char c = (char)readChar;
+
+                    Console.Write(c);
+                }
+            }
+        }
+    }
+}
