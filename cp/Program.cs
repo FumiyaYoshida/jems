24590ceb05a78277c1f82f0da1dc87edcec18ade
jems238 <fu-yoshida@j-ems.jp>
2020-05-26 18:17:32 +0900

    cp3=748 complete step2

--- cp/Program.cs
+++ cp/Program.cs
@@ -6,22 +6,44 @@ namespace mycalc
     {
         static void Main(string[] args)
         {
-            Stream inputStream;
-            if (args.Length == 2 && args[0] == "/i")
+            try
             {
-                inputStream = new FileStream(args[1], FileMode.Open);
+                if (args.Length == 2 && args[0] == "/i")
+                {
+                    try
+                    {
+                        using (var fs = new FileStream(args[1], FileMode.Open))
+                        {
+                            Calclate(fs);
+                        }
+                    }
+                    catch (FileNotFoundException ex)
+                    {
+                        throw new MyAppException("ファイルが見つかりませんでした。", ex);
+                    }
+                }
+                else
+                {
+                    Calclate(Console.OpenStandardInput());
+                }
             }
-            else
+            catch (MyAppException ex)
             {
-                inputStream = Console.OpenStandardInput();
+                Console.Error.WriteLine(ex.Message + "プログラムを終了します。");
             }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("異常が発生しました。プログラムを終了します。\r\n" + ex.Message);
+            }
+        }
+        static void Calclate(Stream inputStream)
+        {
             using (var reader = new StreamReader(inputStream))
             {
                 int readChar;
                 while ((readChar = reader.Read()) != -1)
                 {
                     char c = (char)readChar;
-
                     Console.Write(c);
                 }
             }
